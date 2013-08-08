308f9eb76ced526f4437bb6b3526473dc0c6480b
OpenShift guest <vymiyai@gmail.com>
2013-08-08 00:53:21 -0400

    Primeiro prototipo da biblioteca.

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -3,9 +3,18 @@
 /* Controllers */
 
 angular.module('myApp.controllers', []).
-  controller('MyCtrl1', [function() {
-
-  }])
-  .controller('MyCtrl2', [function() {
-
-  }]);
+    controller('MyCtrl1', [ '$scope', 'Buttons', function( $scope, Buttons ) {
+        $scope.buttons = Buttons.menu;
+    }]).
+    controller('MyCtrl2', [ '$scope', 'Buttons', function( $scope, Buttons ) {
+        $scope.buttons = Buttons.menu;
+    }]).
+    controller('LibraryController', [ '$scope', 'Buttons', 'Profiles',  function( $scope, Buttons, Profiles ) {
+        $scope.buttons = Buttons.menu;
+        $scope.profiles = Profiles;
+        
+        $scope.profileClick = function( characterName ){ $scope.profile = Profiles[ characterName ]; };
+    }]).
+    controller('ProfileController', [ '$scope', 'Profiles', 'LibraryProfile', function( $scope, Profiles, LibraryProfile ) {
+        
+    }]);
--- app/js/services.js
+++ app/js/services.js
@@ -5,5 +5,48 @@
 
 // Demonstrate how to register services
 // In this case it is a simple value service.
-angular.module('myApp.services', []).
-  value('version', '0.1');
+angular.module( 'myApp.services', [] ).
+    value('version', '0.1').
+    service('Buttons', function(){
+        var buttons = {
+            menu:[
+                {
+                    html:"Toggle Precondition 1",
+                    href:"#/view1"
+                },
+                {
+                    html:"Toggle Precondition 2",
+                    href:"#/view2"
+                },
+                {
+                    html:"Library",
+                    href:"#/library"
+                }]
+        };
+        
+        return buttons;
+    }).
+    service('Profiles', function(){
+        var profiles = {
+                "Vats Darkagma": {
+                    name:"Vats Darkagma",
+                    src:"img/ore.png",
+                    description:"The two-kilo whitefish."
+                },
+                "Vennrod Chietzcotl": {
+                    name:"Vennrod Chietzcotl",
+                    src:"img/ore.png",
+                    description:"The Deuterius' proud captain."
+                }
+            };
+        
+        return profiles;
+    }).
+    service('LibraryProfile', function(){
+        var profile = {};
+        
+        return {
+            get:function(){ return profile; },
+            set:function( newProfile ){ profile = newProfile; },
+        };
+    });
